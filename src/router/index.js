192dfd9c20c6f8291531fa2044154adbe7614d23
Laura Reeve <lauralee51@gmail.com>
2018-02-28 17:45:08 -0800

    Uninstalled GA code and installed GA App code in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
+import VueAnalytics from'vue-ua'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Survey from '@/components/Survey'
 import Secret from '@/components/Secret'
 
-Vue.use(Router)
-
-export default new Router({
-  routes: [
+// If you're using vue-router and want route integration, create your routes before enabling vue-ua.
+const router = new VueRouter(
     {
       path: '/',
       name: 'Home',
@@ -24,5 +23,15 @@ export default new Router({
       name: 'Secret',
       component: Secret
     }
-  ]
+)
+
+Vue.use(VueAnalytics, {
+  // [Required] The name of your app as specified in Google Analytics.
+  appName: 'Sample Survey',
+  // [Required] The version of your app.
+  // appVersion: '<app_version>',
+  // [Required] Your Google Analytics tracking ID.
+  trackingId: 'UA-114925962-1',
+  // If you're using vue-router, pass the router instance here.
+  vueRouter: router
 })
